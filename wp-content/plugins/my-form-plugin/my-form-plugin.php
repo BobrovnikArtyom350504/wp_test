14260dfc94764d11241c3c73e182ccf0b13c7154
BobrovnikArtyom350504 <artyonn@gmail.com>
2016-11-16 11:48:55 +0300

    create empty plugin

---
+++ wp-content/plugins/my-form-plugin/my-form-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: My Form Plugin
+ * Plugin URI: http://example.com
+ * Description: This plugin create a form.
+ * Version: 1.0.0
+ * Author: Bobrovnik Artyom
+ * Author URI: http://example.com
+ * License: GPL2
+ */
